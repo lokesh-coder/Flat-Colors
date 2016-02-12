a01e14d19b36ee5e845323b6c2fe92d894926235
Lokesh <lokesh.aero@gmail.com>
2016-02-12 07:03:20 +0530

    Added Html export and random many colors method

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Flat-Colors
-Over 2K of flat colors for your next project.
+Flat UI Colors
+===================
+
+Hand picked **2109** Flat colors for your next project.
+
+----------
+####Install
+```
+npm install --save flat-ui-colors
+```
+
+
+####Usage
+
+```
+var flatcolors=require('flat-ui-colors');
+
+//list all colors
+flatcolors.listAll();
+
+//list random color
+flatcolors.listRandom();
+
+//list random number of colors
+flatcolors.randomMany(5)
+
+//export colors to a nice looking html page :)
+flatcolors.randomManyExport(300);
+```
--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-var uniqueRandomArray = require('unique-random-array');
+var _ = require('lodash');
 var colorList = require('./color-list.json');
+var file = require('easy-file');
+var htmlColorContent = '';
+var htmlFileContent = '';
+
+file.read('export/flat-ui-colors.html', function(contents) {
+	htmlFileContent = contents;
+});
+
+colorList = _.shuffle(colorList);
 
 module.exports = {
-	list: colorList,
-	randomColor: uniqueRandomArray(colorList)
+	listAll: colorList,
+	randomOne: _.sample(colorList),
+	randomMany: function(count) {
+		return _.sampleSize(_.shuffle(colorList), count);
+	},
+	randomManyExport: function(count) {
+		_(this.randomMany(count)).forEach(function(color) {
+			htmlColorContent = htmlColorContent + '<div class="color"><span style="background:' + color + '" class="color-demo"></span><span class="color-name">' + color + '</span></div>';
+		});
+		var content = _.replace(htmlFileContent, '--content--', htmlColorContent);
+		file.write('flat-ui-colors.html', content);
+		return content;
+	},
 };
